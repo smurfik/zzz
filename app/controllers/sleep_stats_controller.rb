47b3cc1982ac33702183d54bb69e0d7834f97da8
smurfik <smurfik@mlpinit.com>
2015-11-23 15:29:12 -0800

    add validation and error messages

--- app/controllers/sleep_stats_controller.rb
+++ app/controllers/sleep_stats_controller.rb
@@ -4,4 +4,10 @@ class SleepStatsController < ActionController::Base
     @sleep_stats = SleepStat.all
   end
 
+  def create
+    @sleep_stat = SleepStat.new
+    @sleep_stat.hours = params[:hours]
+    raise
+  end
+
 end
--- app/models/sleep_stat.rb
+++ app/models/sleep_stat.rb
@@ -1,5 +1,7 @@
 class SleepStat < ActiveRecord::Base
 
+  validates :hours, presence: true
+
   def clean_time
     created_at.strftime("%m-%e-%y %H:%M")
   end
