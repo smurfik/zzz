f030a22854fa6b80f6b435306510ac7a7d9d9ea6
smurfik <smurfik@mlpinit.com>
2015-11-25 11:57:26 -0800

    add validation for mood

--- app/models/sleep_stat.rb
+++ app/models/sleep_stat.rb
@@ -2,6 +2,8 @@ class SleepStat < ActiveRecord::Base
 
   validates :hours, presence: true, numericality: { only_integer: true }
 
+  validates :mood, presence: true
+
   def clean_time
     created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%m-%e-%y %H:%M")
   end
