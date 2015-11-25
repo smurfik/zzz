4c5a8f3ab2a52783d2581e548f839384ffcfbe28
smurfik <smurfik@mlpinit.com>
2015-11-25 15:49:50 -0800

    add javascript to edit btn

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
 
-  $('.btn').first().click(function(){
-    $('.update').first().show();
-    $(this).hide();
+  for(var i=0; i < $('.btn').length; i++) {
 
-  });
+    el = $('.btn')[i]; //Javascript
+    $el = $(el); //JQuery Element
+
+    $el.click(function(){
+      $($('.delete')[4]).hide();
+      $($('.update')[4]).show();
+    });
+    /*
+    $('.btn').firt().click(function(){
+      $('.update').first().show();
+      $(this).hide();
+
+    });
+    */
+  }
 
 });
