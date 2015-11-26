963fe6c72d1b84a2ea7e01d91355d82f8e54a978
smurfik <smurfik@mlpinit.com>
2015-11-25 20:13:47 -0800

    add validation for the mood field

--- app/models/sleep_stat.rb
+++ app/models/sleep_stat.rb
@@ -2,7 +2,7 @@ class SleepStat < ActiveRecord::Base
 
   validates :hours, presence: true, numericality: { only_integer: true }
 
-  validates :mood, presence: true
+  validates :mood, presence: true, format: { with: /\A\D+\z/, message: "only allows letters" }
 
   def clean_time
     created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%m-%e-%y %H:%M")
