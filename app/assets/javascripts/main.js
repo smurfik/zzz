f0efad0c61538a0991bb0500c70dd5420fde112b
smurfik <smurfik@mlpinit.com>
2015-11-27 10:31:32 -0800

    remove the while loop

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,16 +9,6 @@ $(document).ready(function() {
       var id = this.id.split('_')[1];
       $('#update_' + id).show();
       $(this).parent().hide();
-      // $('#edit_' + id).parent().hide();
     });
   }
-
-  i = 0;
-  while (i < $('.btn').length) {
-    // asdlfkkladsfaj
-    i++;
-    i += 1;
-    i = i + 1;
-  }
-
 });
